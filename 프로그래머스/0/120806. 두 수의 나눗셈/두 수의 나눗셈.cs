3bd586936e465f3972229d0997bf2ead24918ffa
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-01-18 08:55:47 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        float de = (float)num1/(float)num2 *1000;
+        
+        return (int)de;
+    }
+}
