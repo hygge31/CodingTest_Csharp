b5547c407ee3bd5f885f2c636443370584f458da
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-01-23 08:56:17 +0900

    [level 0] Title: 배열의 평균값, Time: 0.72 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public double solution(int[] numbers) {
+        double answer = numbers.Sum() / (double)numbers.Length;
+    
+        
+        
+        
+        return answer;
+    }
+}
