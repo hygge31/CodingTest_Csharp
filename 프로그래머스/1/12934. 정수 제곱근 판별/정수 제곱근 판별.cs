9820759948dded9d073152fbdca87b375cf9874f
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-02-05 09:02:54 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -1,16 +1,9 @@
+
 using System;
 public class Solution {
     public long solution(long n) {
-        long answer = 0;
-        long sqrt = (long)Math.Sqrt(n);
-        answer = (long)Math.Pow(sqrt,2);
+        long x = (long)Math.Sqrt(n);
         
-        
-        if(answer ==n){
-            answer = (long)Math.Pow(sqrt+1,2);
-        }else{
-            answer = -1;
-        }
-        return answer;
+            return (x*x == n) ? (x+1)*(x+1) : -1;
     }
 }
