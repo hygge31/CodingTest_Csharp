34450aaf0747b7db873a6990252231abd9008a8d
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-02-07 09:05:07 +0900

    [level 1] Title: 하샤드 수, Time: 2.45 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -3,14 +3,12 @@ using System;
 public class Solution {
     public bool solution(int x) {
         int ad = 0;
-    int[] arr = x.ToString().Select(c=>int.Parse(c.ToString())).ToArray();
+    var sum = x.ToString().ToList().Select(c=>int.Parse(c.ToString())).Sum();
+    
+      
         
-        foreach(int n in arr){
-            ad += n;
-        }
+      
         
-        Console.Write(x);
-        
-        return x % ad == 0 ? true : false;
+        return x%sum == 0 ? true :false;
     }
 }
