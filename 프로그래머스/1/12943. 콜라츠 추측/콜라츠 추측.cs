c5687894cbb7f1aa7f96c07445defae9ac830a13
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-02-13 09:03:28 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public long solution(long num) {
+        long answer = 0;
+        
+        while(num != 1 && answer <500){
+            answer++;
+            
+            if(num %2 == 0){
+                num = num / 2;
+            }else{
+                num = num*3+1;
+            }
+            
+        }
+        
+        return answer == 500 ? -1 : answer;
+    }
+}
