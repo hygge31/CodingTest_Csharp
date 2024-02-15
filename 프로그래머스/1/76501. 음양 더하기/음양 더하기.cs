f70a91fd500bf4119ac810d6e4d8a5a51a69fd47
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-02-16 08:56:57 +0900

    [level 1] Title: 음양 더하기, Time: 1.49 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
@@ -1,16 +1,10 @@
 using System;
-
+using System.Linq;
 public class Solution {
     public int solution(int[] absolutes, bool[] signs) {
         int answer = 0;
-        
-        for(int i = 0; i < signs.Length;i++){
-            if(signs[i]){
-                answer +=absolutes[i];
-            }else{
-                answer +=absolutes[i] * -1;
-            }
-        }
-        return answer;
+       
+        return absolutes.Select((v,idx)=> signs[idx] ? v : -v).Sum();
     }
+    
 }
