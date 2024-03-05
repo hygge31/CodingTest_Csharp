8545e250d5fd144aff71221390def0b861fa29e0
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-03-05 23:34:28 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.20 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
@@ -1,24 +1,15 @@
 using System;
 public class Solution {
     public int[] solution(int n, int m) {
-       
-        int min = Math.Min(n,m);
-        int max = Math.Max(n,m);
-   
-        int gcd = Gcd(max,min);
+
+        int gcd = Gcd(n,m);
         int lcm =  gcd *(n/gcd) * (m/gcd);
         
         return new int[]{gcd,lcm};
     }
     
     int Gcd(int a, int b){
-        if(a%b == 0){
-            return b;
-        }
-        int Q = (a/b);
-        int R = a%b;
-        
-        return Gcd(b,R);
+     return a%b == 0 ? b : Gcd(b,a%b);
     }
      
 }
