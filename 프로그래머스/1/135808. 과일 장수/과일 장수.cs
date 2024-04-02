7268182a9e5fa478e68f91be3e07fc70900e9d2c
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-04-02 22:51:14 +0900

    [level 1] Title: 과일 장수, Time: 1.58 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+     Array.Sort(score);
+      for(int i = score.Length-m;i>=0;i-=m){
+          answer += score[i] * m;
+      }
+     
+          
+        return answer;
+    }
+}
